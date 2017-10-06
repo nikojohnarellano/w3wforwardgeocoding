790a941b77b45a02f347f1b36f6f62d6e5b70d0e
Niko Arellano <narellano@my.bcit.ca>
2017-10-06 00:05:42 -0700

    Initial Commit

---
+++ w3wforwardgeocoding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  w3wforwardgeocoding
+//
+//  Created by Niko Arellano on 2017-10-06.
+//  Copyright © 2017 Mobilux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
